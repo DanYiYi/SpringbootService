7a8789e4bb6436c3f22fa429baeaee0ae4088b37
danyi <994705167@qq.com>
2020-12-29 16:53:24 +0800

    测试3

--- src/main/java/sun/controller/TestController.java
+++ src/main/java/sun/controller/TestController.java
@@ -5,4 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
