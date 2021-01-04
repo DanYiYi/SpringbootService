ed7976d2b9fe5d9d2fe08db85a3c62f86329863e
danyi <994705167@qq.com>
2021-01-04 10:17:50 +0800

    测试13

--- src/main/java/sun/controller/ServiceController.java
+++ src/main/java/sun/controller/ServiceController.java
@@ -29,7 +29,6 @@ public class ServiceController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("111111");
 
         CommUtils.printDataJason(response, restfulResult);
     }
